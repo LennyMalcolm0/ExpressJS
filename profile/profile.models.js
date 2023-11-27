a90668fb5ccc2ba8eff864b0b4d00101f367715f
Leonard-Prosper <leonardmalcolm00@gmail.com>
2023-11-27 01:55:16 +0100

    Created profile schema and model

--- profile/profile.models.js
+++ profile/profile.models.js
@@ -2,38 +2,48 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema
 
-const competitionSchema = new Schema({
-    name: String,
-    prizeMoney: Number
+const imageArraySchema = new Schema({
+    url: { 
+        type: String, 
+        required: true
+    }
 });
 
 const profileSchema = new Schema({
-    name: { 
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    username: { 
         type: String, 
         required: true,
-        minLength: 2
+        match: /^[a-zA-Z0-9_]+$/,
+        lowercase: true
     },
-    club: { 
+    dateOfBirth: { 
+        type: Date, 
+        required: true,
+        validate: {
+            validator: function(date) {
+                const eighteenYearsAgo = new Date();
+                eighteenYearsAgo.setFullYear(eighteenYearsAgo.getFullYear() - 18);
+                return date >= eighteenYearsAgo;
+            },
+            message: "Date is not at least 18 years ago!"
+        }
+    },
+    gender: { type: String, required: true },
+    bio: { type: String, required: true },
+    profilePictureUrl: { type: String, required: true },
+    photos: { type: [imageArraySchema], required: true },
+    interests: { type: [String], required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    referredBy: { type: String, required: false },
+    referralCode: { 
         type: String, 
-        required: true,
+        required: false,
+        unique: true 
     },
-    age: { 
-        type: Number, 
-        required: true,
-        min: 16,
-        max: 45,
-    },
-    goals: { 
-        type: Number, 
-        required: true 
-    },
-    assists: Number,
-    competition: [String],
 }, { timestamps: true })
-
-// playerSchema.methods.SayHello = function() {
-
-// }
 
 const Profile = mongoose.model("Profile", profileSchema);
 
