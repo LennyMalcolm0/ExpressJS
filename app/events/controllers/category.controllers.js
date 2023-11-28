f1392d38a664feee7165b6f9fbd14ccb356c7d70
Leonard-Prosper <leonardmalcolm00@gmail.com>
2023-11-28 12:16:22 +0100

    Added event category controllers

---
+++ app/events/controllers/category.controllers.js
@@ -0,0 +1,78 @@
+const { Category } = require("../event.models");
+const mongoose = require("mongoose");
+
+const getCategories = async (req, res) => {
+    try {
+        const categories = await Category.find({}).lean();
+    
+        res.status(200).send(categories);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+const createCategory = async (req, res) => {
+    const payload = req.body;
+    if (!payload) {
+        return res.status(401).send({ error: "No payload sent" });
+    }
+
+    try {
+        const category = await Category.create({ ...payload });
+
+        res.status(200).send(category);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+const updateCategory = async (req, res) => {
+    const { id } = req.params;
+    const payload = req.body;
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(401).send({ error: "Not a valid id" });
+    }
+    if (!payload) {
+        return res.status(401).send({ error: "No payload sent" });
+    }
+
+    try {
+        const category = await Category
+            .findByIdAndUpdate(
+                id,
+                { ...payload },
+                { new: true, lean: true }
+            );
+    
+        if (!category) {
+            return res.status(404).send({ error: "Category not found" });
+        }
+    
+        res.status(200).send(category);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+const deleteCategory = async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(401).send({ error: "Not a valid id" });
+    }
+
+    try {
+        await Category.findByIdAndDelete(id);
+        res.status(200).send("Success");
+    } catch (error) {
+        res.status(400).send(error);
+    }
+}
+
+module.exports = {
+    getCategories,
+    createCategory,
+    updateCategory,
+    deleteCategory,
+}
